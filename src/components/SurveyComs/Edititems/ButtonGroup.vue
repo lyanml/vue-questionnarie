fac80dcb0bdaf5cf8ca37c29df5ccb0879ffc29b
lyanml <liuyanming2018@gmail.com>
2025-01-06 08:09:31 +0800

    封装ButtonGroup组件

---
+++ src/components/SurveyComs/Edititems/ButtonGroup.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div class="flex align-items-center space-between">
+      <div class="flex align-items-cneter">
+        <div class="mr-20">{{ title }}</div>
+        <div class="currentStatus">{{ status }}</div>
+      </div>
+      <div>
+        <slot />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    default: '',
+  },
+  status: {
+    type: String,
+    default: '',
+  },
+})
+</script>
+<style lang="scss" scoped>
+.currentStatus {
+  color: var(--info-color);
+  font-size: var(--font-size-base);
+}
+</style>
--- src/components/SurveyComs/Edititems/PositionEditor.vue
+++ src/components/SurveyComs/Edititems/PositionEditor.vue
@@ -1,30 +1,23 @@
 <template>
-  <div>
-    <div class="flex align-items-center space-between">
-      <div class="flex align-items-cneter">
-        <div class="mr-20">居中设置</div>
-        <div class="currentStatus">{{ status[currentStatus] }}</div>
-      </div>
-      <div>
-        <el-button-group>
-          <el-button :class="{ select: currentStatus === 0 }" @click="changePosition(0)">
-            <font-awesome-icon icon="align-left"></font-awesome-icon>
-          </el-button>
-          <el-button
-            :class="{
-              select: currentStatus === 1,
-            }"
-            @click="changePosition(1)"
-          >
-            <font-awesome-icon icon="align-center" />
-          </el-button>
-        </el-button-group>
-      </div>
-    </div>
-  </div>
+  <ButtonGroup title="居中设置" :status="status[currentStatus]">
+    <el-button-group>
+      <el-button :class="{ select: currentStatus === 0 }" @click="changePosition(0)">
+        <font-awesome-icon icon="align-left"></font-awesome-icon>
+      </el-button>
+      <el-button
+        :class="{
+          select: currentStatus === 1,
+        }"
+        @click="changePosition(1)"
+      >
+        <font-awesome-icon icon="align-center" />
+      </el-button>
+    </el-button-group>
+  </ButtonGroup>
 </template>
 
 <script setup lang="ts">
+import ButtonGroup from './ButtonGroup.vue'
 import type { StringStatusArr, UpdateStatus } from '@/types'
 import { inject } from 'vue'
 
